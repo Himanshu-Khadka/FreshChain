f7ef7f405acb98e8b9452bb5ae5ba8b2909a5842
Himanshu Khadka <iamhimanshu.me@gmail.com>
2024-07-16 14:21:32 -0600

    Hashing Method

--- src/main/java/himanshukhadka/StringUtil.java
+++ src/main/java/himanshukhadka/StringUtil.java
@@ -4,12 +4,13 @@ import java.security.MessageDigest;
 public class StringUtil {
     public static String applysha256(String input){
         try{
-            MessageDigest digest = MessageDigest.getInstance("SHA-256");
-            byte[] hash = digest.digest(input.getBytes("UTF-8"));
-            StringBuffer hexString = new StringBuffer();
+
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");//Creates a SHA-256 hash
+            byte[] hash = digest.digest(input.getBytes("UTF-8"));//Applies sha256 to our input
+            StringBuffer hexString = new StringBuffer();// This will contain hash as hexidecimal
             for (int i = 0; i < hash.length; i++){
-                String hex = Integer.toHexString(0xff & hash[i]);
-                if (hex.length() == 1) hexString.append(0);
+                String hex = Integer.toHexString(0xff & hash[i]); //Converts byte to integer
+                if (hex.length() == 1) hexString.append(0); //If hex is less than 2 digits, add a 0
                 hexString.append(hex);
             }
             return hexString.toString();
